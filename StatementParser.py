b039a3127308b4ebdca62c774c1e4eb378e8749a
iqbal <iqbal.rasheed@tprc.nl>
2018-06-07 17:29:28 +0200

    dded statement parser and dump file for trials.

--- StatementParser.py
+++ StatementParser.py
@@ -2,7 +2,7 @@ import re
 class SParser:
     s=None
     fl=['is-a','is','has','with', 'and', 'consists-of', 'comprises']
-    sl=['sub-component-of', 'connected-to',
+    sl=['sub-component-of', 'connected-to','related-to',
         'property', 'function', 'attribute']
     k=[]
     delimiter=';'
@@ -24,9 +24,26 @@ class SParser:
     def splitKeyword(self,s):
         skk= [self.delimit(st,delimiter='|'.join(self.fl)) for st in s]
         return skk
+    
+    def tokens(self,s):
+        d=self.delimit(s)
+        t=[self.delimit(i,'\s') for i in d]
+        return t
+    
+    def keywords_SVO(self,s):
+        print(s[0])
+        #a=re.finditer('?P<name>\[A-Za-z]+\sis-a\s',s[0])
+        #print(a)
+        #return a
+            
+        
 
 if __name__=="__main__":
     a='FESS is-a Thing; FESS has property energyCapacity[E];'
     sp=SParser(k=['comprises'])
     sp.parse(a)
+    t=sp.tokens(a)
+    print(t)
+    print(sp.keywords_SVO(t))
+    
         
--- dump.py
+++ dump.py
@@ -1,3 +1,5 @@
+"""
+# multilevel dict and nodes
 a={'p':{'1':11,'2' :22},
    'f':{'3':33,'4':44,'5':55}
    }
@@ -13,3 +15,42 @@ n=[Node(str(i)) for i in range(10)]
 print([str(j) for j in n])
 n[0].addProp('yolo')
 print(n[0].getProp())
+"""
+# text parsing
+import re
+import collections
+a='FESS is-a Thing; FESS has property energyCapacity[E];'
+c='rotor is-a sub-component-of FESS and is related-to motor'
+#  id     k1     k2             id   c1  k1     k2       id
+c2='magnet is-a sub-component-of rotor whose thickness[tm,val] is conneted-to elecLoss'
+b='FESS is-a Thing'
+#s=re.match('(P<first_name>\w+) (P<last_name>\w+)',b)
+#print(s)
+
+k1=['is-a','is','has', 'consists-of', 'comprises']
+k2=['sub-component-of', 'connected-to','related-to',
+        'property', 'function', 'attribute']
+c1=['and','which','with','whose']
+tspec=[('ID', r'[A-Z-a-z\[\w+\]]+'),('newline', r'\n')]
+tregx='|'.join('(?P<%s>%s)' % pair for pair in tspec)
+print(tregx)
+Token=collections.namedtuple('Token',['typ','value','level'])
+#mo=re.finditer(tregx,c)
+y=[]
+
+for mo in re.finditer(tregx,c2):
+    k=mo.lastgroup
+    v=mo.group(k)
+    #print(k)
+    if k=='ID':
+        k=v       
+        if v in k1:
+           level='k1'
+        elif v in k2:
+            level='k2'
+        elif v in c1:
+            level='c1'
+        else:
+            level=0
+    y.append(Token(k,v,level))
+print(y)
